4d8ed6a901b0769fe925bea06e77ac6de85a7958
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-23 11:25:10 +0530

    add basic print and assignment

---
+++ FirstProgram.py
@@ -0,0 +1,33 @@
+# python is implicit typed language means usko datatype batana nai padta
+# print("Chirag Prajapati","My age is 20")
+# print(23)
+# print(23+35)
+
+
+# name = "chirag" # String
+# name1 = 'chirag'
+# name2 = '''chirag'''
+# age = 21  # int
+# e = 21.955 #float
+# old = False #boolean
+# a = None #NoneType
+# print(name)
+# print(age)
+# print(e)
+
+# print("my name is : " , name, ", my age is : " , age)
+# print("my age is : " , age)
+
+"""print(type(name))
+print(type(name1))
+print(type(name2))
+print(type(age))
+print(type(e)) 
+print(type(old)) 
+print(type(a)) """
+
+c = 200
+d = 552
+sum = c + d
+print(sum)
+ 
