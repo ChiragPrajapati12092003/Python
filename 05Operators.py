78a2c1a3dd39d2bf509b0dac0fc01a65f2bf23a6
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-23 20:06:47 +0530

    info about all operators

---
+++ 05Operators.py
@@ -0,0 +1,56 @@
+# Arithmatic Operators
+
+'''
+a = 5
+b = 2
+
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b) #always gives float value
+print (a % b) #remainder
+print (a ** b) #a^b
+
+'''
+'''
+
+# Relational Operators
+
+c = 50
+d = 20
+
+print(c == d) #False
+print(c != d) #True
+print(c >= d) #True
+print(c > d) #True
+print(c <= d) #False
+print(c < d) #False
+
+'''
+
+#Assignment Operators
+
+num = 10
+# num = num + 10 #10+10 => 20
+# num += 10
+# num -= 10
+# num *= 10
+# num /= 5
+num **= 2
+# print("num : ",num)
+
+
+# Logical Operators
+
+x = 50
+y = 30
+print(not False)
+print(not True)
+print(not (x > y))
+
+val1 = True
+val2 = False
+print("and operator : ",val1 and val2)
+print("or operator : ",val1 or val2)
+print("OR operator :",(x == y) or (x > y))
+
