2e4347b9d1c1107bb591dc299039a5a88c887896
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-23 12:08:50 +0530

    Input Type is added

---
+++ 03Input.py
@@ -0,0 +1,6 @@
+#Taking input from user & print it
+name = input("name : ")
+age = int(input("age :"))
+price = float(input("price :"))
+
+print("My name is :" ,name , ", My age is :" ,age , ", my toy price is :" ,price)
