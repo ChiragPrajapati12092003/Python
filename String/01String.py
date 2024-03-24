9d058e64fb81d020fa4ea1483794066ec5a81cef
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-24 12:52:50 +0530

    string and its some operations

---
+++ String/01String.py
@@ -0,0 +1,23 @@
+# str1 = "This is string"
+# str2 = 'Chirag'
+# str3 = '''this is also string'''
+
+# # str4 = "This is a string. \n we are creating it on python \t my name is chirag "
+# # print(str4)
+
+# str5 = "chirag"
+# str6 = "prajapati"
+# final_String = str5 + "" + str6
+# print(final_String)
+
+# print(len(str5))
+
+# str = "apna collage"
+# print(str[1:4]) #here 1 to 3 will print ans=pna
+# print(str[:4])#starting to 3
+# print(str[5:])#5 to end of string
+# print(str[5:len(str)]) #gives upto last word
+
+str7 = "apple"
+print(str7[-2:-1]) #doent contains -1 vala word ans=l
+print(str7[-1:-2]) #doent gives output bcz from -1 word se right side jate time koi bhi index -2 hai hi nai agar string khatam ho jaye to yeh vaha hi khatam ho jata hai
