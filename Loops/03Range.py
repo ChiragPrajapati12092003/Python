52d496d40ab977277ee7e0ffbfd30e10a5912303
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-25 20:43:03 +0530

    basic of range

---
+++ Loops/03Range.py
@@ -0,0 +1,48 @@
+#Range returns a sequence of numbers, starting from 0 by default, and increments by 1(by deafault),and stops befofre a specified numbers
+# range(start?,stop,Step?)
+#ends print nai hoga
+# print(range(5))
+
+# seq = range(10)
+
+# # print(seq[0])
+# # print(seq[1])
+# # print(seq[2])
+# # print(seq[3])
+# # print(seq[4])
+
+# for i in seq:
+#     print(i)
+
+#Or we can write as
+
+# for j in range(10): #range stops at 9
+#     print(j)
+
+# for k in range(2,10): #range starts 2 and end 9
+#     print(k)
+
+# for p in range(2,10,2): # range(start,stop,step)
+#     print(p)
+
+
+#print multiplication table using range
+# n = int(input("Enter the number :"))
+
+# for q in range(1,11):
+#     print(n*q)
+
+
+
+# -----------------Pass in range--------------
+#it is use as futur space means agar ham kabhi koi code us loop mai likhna chahe to
+
+for c in range(5):
+    #empty
+    pass
+
+
+if c > 5:
+    pass
+          
+print("some useful work")
