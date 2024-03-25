b38049a348058b4dadb26b819dddf716617ae61f
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-25 18:52:37 +0530

    while basics

---
+++ Loops/01While.py
@@ -0,0 +1,33 @@
+# count = 1    #koi variable chahiye aur isko iterators kehte hai and is loop process ko iteration kehte hai
+# while count <= 5:         #means jab tak
+#     print("hello")
+#     count +=1
+
+# while True:
+#     print("chirag")     # print infinite chirag
+
+# i = 1
+# while i<=5:
+#     print("apnacollage", i)
+#     i +=1
+
+#print number from 1 to 5
+j = 1
+while j<= 6:
+    print(j)
+    j +=1
+print("Loop ended") 
+
+
+k = 6
+while k>= 1:
+    print(k)
+    k -=1
+print("Loop ended")    
+
+
+# k = 6
+# while k< 7:
+#     print(k)
+#     k -=1
+# print("Loop ended")    #this loop goes into infinite loop which is not useable for real life projects
