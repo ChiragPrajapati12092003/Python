a2227e6114d927944c1840c9da04014df609c6ec
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-25 10:36:22 +0530

    1 que. is solved

---
+++ ListAndTouple/05Practice.py
@@ -0,0 +1,31 @@
+# WAP to ask the user to enter names of their 3 fav.  movies & store them in a list
+
+# movies = []
+# mov1 = input("enter 1st movie :")
+# mov2 = input("enter 2nd movie :")
+# mov3 = input("enter 3rd movie :")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+#another way
+# favmov = []
+# mov = input("enter 1st movie: ")
+# favmov.append(mov)
+# mov = input("enter 2nd movie: ")
+# favmov.append(mov)
+# mov = input("enter 3rd movie: ")
+# favmov.append(mov)
+
+# print(favmov)
+
+#another way
+favrmov = []
+favrmov.append(input("enter 1st movie :"))
+favrmov.append(input("enter 2nd movie :"))
+favrmov.append(input("enter 3rd movie :"))
+
+print(favrmov)
