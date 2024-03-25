e5b5453c7a9db9d9de67ba3504ad6733bef8a165
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-25 20:54:29 +0530

    while and for loop questions

---
+++ Loops/04AllPr.py
@@ -0,0 +1,27 @@
+# WAP to find sum of first n natural nmbers
+
+# n = int(input("Enter the number :"))
+# sum = 0
+# for i in range(1,n+1):
+#     sum += i
+# print("Total sum is :",sum)   
+
+
+#using while loop
+# n = int(input("Enter the number :"))
+# sum = 0
+# j = 1
+# while j <= n:
+#     sum += j
+#     j += 1
+# print("Total sum is :",sum)      
+
+
+#WAP to find factorial of first n natural numbers
+
+n1 = int(input("Enter the number :"))
+fac = 1
+for k in range(1,n1+1):
+    fac *= k
+    k += 1
+print("factoroal is : " , fac)
