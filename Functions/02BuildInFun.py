74b28aa554c83dd40fd6947fa1eddd6d748a6c77
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-26 11:02:06 +0530

    some build in func

---
+++ Functions/02BuildInFun.py
@@ -0,0 +1,26 @@
+#print is also buildin function 
+
+#print maens : 
+# (function) def print(
+#     *values: object,
+#     sep: str | None = " ",
+#     end: str | None = "\n",
+#     file: SupportsWrite[str] | None = None,
+#     flush: Literal[False] = False
+# ) -> None
+
+
+print("chirag" ,end=" ")  #it prints both lines in same line bcz by defaut end = "/n"
+print("prajapati")
+
+print("chirag" ,end="$")   #prints $ between to print outputs
+print("prajapati")
+
+
+# len()
+
+# range()
+
+# type()
+
+#user defined functions are create by user or programmer
