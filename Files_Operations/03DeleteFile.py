2966b99c3b945e4040c9412fe733c2cee09226e8
ChiragPrajapati12092003 <chiragprajapati9232@gmail.com>
2024-03-27 13:56:03 +0530

    delete with module

---
+++ Files_Operations/03DeleteFile.py
@@ -0,0 +1,8 @@
+#here direct delete method is not available so we have to do it by import module which is os module
+
+# some are preinstalled but some arenot available in python which are tensorflow
+# nd if we want to install any module then we have to write "pip install modulename" or "pip3 install modulename"
+
+import os
+# os.remove(filename)
+os.remove("D:\Python_Codes\Python\Files_Operations\Delete.txt")
